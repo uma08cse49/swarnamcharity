4d62143ec6c4e5e7ca9d82433418cdf2703a6609
uma <uma08cse49@gmail.com>
2025-11-03 11:17:46 +0530

    03-11-2025

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,63 @@ document.addEventListener('DOMContentLoaded', function(){
   // initial call
   reveal();
 });
+
+
+// =========================================================================================
+
+// Learn more button to funtion
+
+
+// document.getElementById("learnMoreBtn").addEventListener("click", function() {
+//   const moreContent = document.getElementById("moreContent");
+//   if (moreContent.style.display === "block") {
+//     moreContent.style.display = "none";
+//     this.textContent = "Learn More";
+//   } else {
+//     moreContent.style.display = "block";
+//     this.textContent = "Show Less";
+//   }
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const learnMoreBtn = document.getElementById("learnMoreBtn");
+//   const moreContent = document.getElementById("moreContent");
+
+//   if (learnMoreBtn && moreContent) {
+//     learnMoreBtn.addEventListener("click", function() {
+//       moreContent.classList.toggle("show");
+//       learnMoreBtn.textContent = moreContent.classList.contains("show")
+//         ? "Show Less"
+//         : "Learn More";
+//     });
+//   }
+// });
+
+
+// ================================================================================================
+
+
+
+// js for mulitple learn more button to function
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.querySelectorAll(".btn-learn-more");
+
+  buttons.forEach(function(btn) {
+    btn.addEventListener("click", function() {
+      const hiddenContent = this.previousElementSibling; // get the <div class="hidden-content">
+
+      if (hiddenContent.style.display === "none" || hiddenContent.style.display === "") {
+        hiddenContent.style.display = "block";
+        this.textContent = "Show Less";
+      } else {
+        hiddenContent.style.display = "none";
+        this.textContent = "Learn More";
+      }
+    });
+  });
+});
+
+
